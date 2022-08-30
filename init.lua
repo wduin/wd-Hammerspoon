2e22a7e8e2198e084c840d7da173662e3d22105c
TrayvonZhao <trayvon.zhao@zoom.us>
2022-08-30 18:10:35 +0800

    feat:修改输入法

--- init.lua
+++ init.lua
@@ -4,27 +4,35 @@ require "modules/resize"
 require "modules/volume"
 --require "modules/reload"
 require "modules/bluetooth"
+require "modules/input"
 
 ----二分之一
 --hotkey.bind(hyper, "A", leftHalf)
 --hotkey.bind(hyper, "D", rightHalf)
 --hotkey.bind(hyper, "W", topHalf)
 --hotkey.bind(hyper, "S", bottomHalf)
---
+
 ----三分之一
 --hs.hotkey.bind(hyper, "o", verticalThird)
 --hs.hotkey.bind(hyper, "p", horizontalThird)
---
+
 ----四分之一
 --hotkey.bind(hyper, "J", leftQuarter)
 --hotkey.bind(hyper, "L", rightQuarter)
 --hotkey.bind(hyper, "I", topQuarter)
 --hotkey.bind(hyper, "K", bottomQuarter)
+
 ----全屏
 --hotkey.bind(hyper, 'F', fullscreenNotResize)
 --居中
 ----hotkey.bind(hyper, 'C', centerNoResize)
---
+
+-- 移动鼠标到其他屏幕
+--hotkey.bind(hyper, "/", function()
+--    focusScreen(window.focusedWindow():screen():previous())
+--end)
+
+
 ----全屏,单独一个窗口
 hotkey.bind(hyperShift, 'F', fullscreenWillResize)
 --居中四分之一
@@ -35,7 +43,9 @@ hotkey.bind(hyper, 'B', centerHalf)
 hs.hotkey.bind(hyper, '[', changeVolume(-3))
 hs.hotkey.bind(hyper, ']', changeVolume(3))
 
--- 移动鼠标到其他屏幕
---hotkey.bind(hyper, "/", function()
---    focusScreen(window.focusedWindow():screen():previous())
---end)
+-- 展示所有屏幕的应用,可以快捷选择应用
+hotkey.bind(hyper, '.', function()
+    hints.windowHints()
+    -- Display current application window
+    -- hints.windowHints(hs.window.focusedWindow():application():allWindows())
+end)
